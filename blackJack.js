14104e63ef560701ef789b34b1a2c14ae9efe172
iancorbett <iancorbett1324@gmail.com>
2025-09-06 14:24:44 -0700

    make createdeck function, iterates through each card, in each suit, in each deck until we have a playable deck with 312 cards

--- blackJack.js
+++ blackJack.js
@@ -1,2 +1,13 @@
 const SUITS = ["♠", "♥", "♦", "♣"];
 const RANKS = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+function createDeck(n=6) { //using six decks, as most casinos use 6 or 8 in real life
+    const d = []; //create deck starting with empty array
+    for (let i=0;i<n;i++) {//run for loop  once for all 6 decks
+      for (const s of SUITS) {//iterate through each of 4 suits once for each deck
+        for (const r of RANKS) d.push({ r, s }); //iterate through each of the thirteen cards for each suit in each deck, 52 cards per deck, 312 cards in total
+      }
+    }
+   
+  }
+  
